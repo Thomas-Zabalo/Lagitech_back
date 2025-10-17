828d669a4bde1dec78ec0db47ce980d8bc9f02e7
Evboys <evanboy@hotmail.fr>
2025-10-17 17:50:02 +0200

    +

--- src/main/java/com/workshop/lagitech/controllers/UserController.java
+++ src/main/java/com/workshop/lagitech/controllers/UserController.java
@@ -13,19 +13,42 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
     @GetMapping("/team/{teamId}")
     public List<User> getUsersByTeam(@PathVariable Long teamId) {
         return userService.getUsersByTeam(teamId);
     }
+
     @GetMapping("/email")
     public Optional<User> getUserByEmail(@RequestParam String email) {
         return userService.getUserByEmail(email);
     }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+    }
+
+    @PutMapping("/{id}")
+    public User updateUserById(@PathVariable Long id, @RequestBody User updatedUser) {
+        User currentUser = userService.getUserById(id)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+
+        // Mettre à jour uniquement les champs autorisés
+        currentUser.setName(updatedUser.getName());
+        currentUser.setEmail(updatedUser.getEmail());
+
+        return userService.updateUser(id,currentUser);
+    }
+
 }
